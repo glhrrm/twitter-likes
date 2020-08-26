4935d2139328d02b5399c57271994bf3cdd7d94c
gui.parzianello <glhrm_@live.com>
2020-08-26 20:25:39 -0300

    architectural improvements & method refactorings

--- backend/api/like.js
+++ backend/api/like.js
@@ -1,66 +1,50 @@
-const fetch = require('node-fetch')
-
-const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAIQjHAEAAAAADcYypaf6RxoMgMbK%2FVFgx3waopI%3DW7WqrlkuO8uwUzhKl73uDUbprSKdQy36BmsLPtCTIoZadwPyyc'
-
-const api = (screenName, count) =>
-    fetch('https://api.twitter.com/1.1/favorites/list.json?'
-        + new URLSearchParams({
-            count: count,
-            screen_name: screenName,
-        }), {
-        headers: {
-            'Authorization': `Bearer ${bearerToken}`
-        }
-    }).then(res => res.json()).catch(err => res.send(err))
+const { apiCall } = require('../config/api')
 
 module.exports = app => {
     const find = (req, res) => {
         const user = req.params.user
 
-        api(user, 20)
-            .then(data => res.send(data.map(tweet => {
-                if (tweet.hasOwnProperty('extended_entities')) {
-                    const username = tweet.user.screen_name
+        apiCall(user, 20)
+            .then(data => res.send(data.filter(tweet =>
+                tweet.hasOwnProperty('extended_entities')
+                && tweet.extended_entities.hasOwnProperty('media')
+            ).map(tweet => {
+                const username = tweet.user.screen_name
 
-                    const profileImage = tweet.user.profile_image_url.replace('_normal.', '.')
+                const profileImage = tweet.user.profile_image_url.replace('_normal.', '.')
 
-                    const tweetUrl = `http://twitter.com/${username}/status/${tweet.id_str}`
+                const tweetUrl = `http://twitter.com/${username}/status/${tweet.id_str}`
 
-                    const images = []
+                const images = []
 
-                    if (tweet.extended_entities.hasOwnProperty('media')) {
-                        tweet.extended_entities.media.map(m => images.push(m.media_url))
-                    }
+                tweet.extended_entities.media.map(m => images.push(m.media_url))
 
-                    return { username, profileImage, tweetUrl, images }
-                }
-            }).filter(result => result != null)))
+                return { username, profileImage, tweetUrl, images }
+            })))
             .catch(err => res.send(err))
     }
 
     const findByUser = (req, res) => {
         const { user, likedUser } = req.params
 
-        api(user, 20)
-            .then(data => res.send(data.map(tweet => {
-                if (tweet.hasOwnProperty('extended_entities')) {
-                    const username = tweet.user.screen_name
+        apiCall(user, 20)
+            .then(data => res.send(data.filter(tweet =>
+                tweet.hasOwnProperty('extended_entities')
+                && tweet.extended_entities.hasOwnProperty('media')
+                && tweet.user.screen_name.toLowerCase() == likedUser.toLowerCase()
+            ).map(tweet => {
+                const username = tweet.user.screen_name
 
-                    if (username.toLowerCase() == likedUser.toLowerCase()) {
-                        const profileImage = tweet.user.profile_image_url.replace('_normal.', '.')
+                const profileImage = tweet.user.profile_image_url.replace('_normal.', '.')
 
-                        const tweetUrl = `http://twitter.com/${username}/status/${tweet.id_str}`
+                const tweetUrl = `http://twitter.com/${username}/status/${tweet.id_str}`
 
-                        const images = []
+                const images = []
 
-                        if (tweet.extended_entities.hasOwnProperty('media')) {
-                            tweet.extended_entities.media.map(m => images.push(m.media_url))
-                        }
+                tweet.extended_entities.media.map(m => images.push(m.media_url))
 
-                        return { username, profileImage, tweetUrl, images }
-                    }
-                }
-            }).filter(result => result != null)))
+                return { username, profileImage, tweetUrl, images }
+            })))
             .catch(err => res.send(err))
     }
 
---
+++ backend/config/api.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch')
+
+const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAIQjHAEAAAAADcYypaf6RxoMgMbK%2FVFgx3waopI%3DW7WqrlkuO8uwUzhKl73uDUbprSKdQy36BmsLPtCTIoZadwPyyc'
+
+const apiCall = (screenName, count) =>
+    fetch('https://api.twitter.com/1.1/favorites/list.json?'
+        + new URLSearchParams({
+            count: count,
+            screen_name: screenName,
+        }), {
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        }
+    }).then(res => res.json()).catch(err => res.send(err))
+
+module.exports = { bearerToken, apiCall }
